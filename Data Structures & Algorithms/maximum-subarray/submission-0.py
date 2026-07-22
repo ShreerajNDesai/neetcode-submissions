03da0eae96cf3077eda8fb859af621084ab5ecea
Shreeraj N Desai <131637507+ShreerajNDesai@users.noreply.github.com>
2026-07-22 18:37:52 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if not nums:
+            return 0 
+
+        current_max = nums[0]
+        global_max = nums[0]
+
+        for i in range(1, len(nums)):
+            num = nums[i]
+            current_max = max(num, current_max + num)
+            global_max = max(global_max, current_max)
+            
+        return global_max
